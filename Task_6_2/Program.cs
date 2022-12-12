6960fd89234c8a8e5ef4878f5b26138668265987
SaveLuciferR <cmudrec@mail.ru>
2022-12-12 21:23:55 +0300

    Добавьте файлы проекта.

---
+++ Task_6_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_6_2
+{
+	class Program
+	{
+		static void Main()
+		{
+			try
+			{
+				double[] mas;
+				Console.Write("Введите размер массива: ");
+				int n = Convert.ToInt32(Console.ReadLine());
+
+				if (n < 1) throw new Exception();
+				
+				mas = new double[n];
+
+				Console.WriteLine("Введите элементы массива");
+				for (int i = 0; i < n; i++)
+				{
+					mas[i] = Convert.ToDouble(Console.ReadLine());
+				}
+
+				int index = 0;
+				double max = mas[0];
+				for (int i = 1; i < n; i++)
+				{
+					if (max <= mas[i])
+					{
+						max = mas[i];
+						index = i;
+					}
+				}
+
+				Console.WriteLine("Номер последнего максимального значения: {0}", index);
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Введены некорректные значения");
+			}
+			catch
+			{
+				Console.WriteLine("Кол-во элементов массива должен быть больше 0");
+			}
+		}
+	}
+}
